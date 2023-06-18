2622229e486541223760b818127f89793fd54901
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-18 13:25:36 +0430

    add user basket and profile screens

---
+++ lib/screens/user/basket.dart
@@ -0,0 +1,233 @@
+import 'package:bookstore_app/constants/custom_colors.dart';
+import 'package:bookstore_app/widgets/custom_header.dart';
+import 'package:bookstore_app/widgets/custom_page_container.dart';
+import 'package:flutter/material.dart';
+
+class BasketScreen extends StatelessWidget {
+  const BasketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            CustomScrollView(
+              slivers: [
+                //Header Section
+                const SliverPadding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 20,
+                  ),
+                  sliver: SliverToBoxAdapter(
+                    child: CustomHeader(
+                      title: 'سبد خرید',
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(bottom: 65),
+                  sliver: SliverGrid.builder(
+                    itemCount: 6,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 0,
+                      mainAxisExtent: 250,
+                    ),
+                    itemBuilder: (context, index) {
+                      return const BasketProductCart();
+                    },
+                  ),
+                )
+              ],
+            ),
+            //Buy Button Section
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                width: double.infinity,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    top: BorderSide(
+                      width: 2,
+                      color: CustomColors.mainGrayText,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('تکمیل فرایند خرید'),
+                    ),
+                    const Spacer(),
+                    Text(
+                      '500000 تومان',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BasketProductCart extends StatelessWidget {
+  const BasketProductCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 30,
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 2,
+            color: CustomColors.mainGrayText,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 120,
+                height: 150,
+                color: CustomColors.textFieldBackground,
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'عادت های اتمی',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.shield_outlined,
+                        color: CustomColors.mainGrayText,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'ضمانت سلامت کالا',
+                        style: Theme.of(context).textTheme.displaySmall,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.delivery_dining_outlined,
+                        color: CustomColors.mainGrayText,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'ارسال سریع',
+                        style: Theme.of(context).textTheme.displaySmall,
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 120,
+                height: 30,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: CustomColors.mainGrayText,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      padding: const EdgeInsets.all(0),
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    Text(
+                      '1',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      padding: const EdgeInsets.all(0),
+                      icon: const Icon(
+                        Icons.delete_outline,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '20000تومان تخفیف',
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                  Text(
+                    '20000تومان',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/user/profile.dart
@@ -0,0 +1,240 @@
+import 'package:bookstore_app/constants/custom_colors.dart';
+import 'package:bookstore_app/widgets/custom_header.dart';
+import 'package:bookstore_app/widgets/custom_page_container.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPageContainer(
+      elements: [
+        //Header Section
+        const SliverPadding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 20,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: CustomHeader(
+              title: 'حساب کاربری',
+            ),
+          ),
+        ),
+        //Prpfile Image Section
+        SliverToBoxAdapter(
+          child: CircleAvatar(
+            backgroundColor: CustomColors.mainGrayText,
+            maxRadius: 50,
+            minRadius: 40,
+          ),
+        ),
+        //Profile Info Section
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 15,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Text(
+                  'مهدی حیدری',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                Text(
+                  '09214840491',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+              ],
+            ),
+          ),
+        ),
+        //Address Section
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'آدرس من',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('ویرایش'),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 43,
+                  decoration: BoxDecoration(
+                    color: CustomColors.textFieldBackground,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'تهران،منطقه11،خیابان شیخ هادی،پلاک215',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        //Address Section
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'کد پستی من',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('ویرایش'),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 43,
+                  decoration: BoxDecoration(
+                    color: CustomColors.textFieldBackground,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '2541869745',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ),
+                ),
+                Text(
+                  'تمامی اطلاعات کاربران نزد بوک استور محفوظ میباشد.',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.displayMedium,
+                )
+              ],
+            ),
+          ),
+        ),
+        //Wallet Info Section
+        const WalletInfo(),
+        //SignOut Button Section
+        const SignOutButton()
+      ],
+    );
+  }
+}
+
+class WalletInfo extends StatelessWidget {
+  const WalletInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 30,
+      ),
+      sliver: SliverToBoxAdapter(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 2,
+              margin: const EdgeInsets.only(
+                bottom: 10,
+              ),
+              color: CustomColors.mainGrayText,
+            ),
+            Text(
+              'موجودی حساب',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '250000 تومان',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('افزایش موجودی'),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 2,
+              margin: const EdgeInsets.only(
+                top: 10,
+              ),
+              color: CustomColors.mainGrayText,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignOutButton extends StatelessWidget {
+  const SignOutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      sliver: SliverToBoxAdapter(
+        child: Container(
+          width: double.infinity,
+          height: 43,
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: CustomColors.textFieldBackground,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'خروج از حساب',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.power_settings_new_outlined,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
