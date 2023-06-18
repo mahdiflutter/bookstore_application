d4aa6e47d7ec3502abdd2103445070097f25b742
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-18 13:24:55 +0430

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:bookstore_app/constants/custom_colors.dart';
 import 'package:bookstore_app/screens/authentication/login_1.dart';
 import 'package:bookstore_app/screens/authentication/login_2.dart';
 import 'package:bookstore_app/screens/categories.dart';
 import 'package:bookstore_app/screens/home.dart';
 import 'package:bookstore_app/screens/products_by_category.dart';
+import 'package:bookstore_app/screens/user/basket.dart';
+import 'package:bookstore_app/screens/user/profile.dart';
 import "package:flutter/material.dart";
 
 void main() {
@@ -17,43 +20,46 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontFamily: 'vazirbold',
-            fontSize: 18,
-            color: Colors.black,
-          ),
-          displayMedium: TextStyle(
-            color: Colors.black,
-            fontFamily: 'vazirlight',
-            fontSize: 16,
-          ),
-          displaySmall: TextStyle(
-            fontFamily: 'vazirlight',
-            color: Color(0xff858585),
-            fontSize: 14,
-          ),
-          labelLarge: TextStyle(
-            color: Colors.white,
-            fontFamily: 'vazirbold',
-            fontSize: 16,
-          ),
-          titleLarge: TextStyle(
-            color: Colors.white,
-            fontFamily: 'vazirbold',
-            fontSize: 30,
-          ),
-          titleMedium: TextStyle(
-            fontFamily: 'vazirbold',
-            color: Colors.black,
-            fontSize: 16,
-          ),
-          titleSmall: TextStyle(
-            fontFamily: 'vazirlight',
-            fontSize: 14,
-            color: Colors.black,
-          ),
-        ),
+        textTheme: TextTheme(
+            displayLarge: const TextStyle(
+              fontFamily: 'vazirbold',
+              fontSize: 18,
+              color: Colors.black,
+            ),
+            displayMedium: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'vazirlight',
+              fontSize: 16,
+            ),
+            displaySmall: const TextStyle(
+              fontFamily: 'vazirlight',
+              color: Color(0xff858585),
+              fontSize: 14,
+            ),
+            labelLarge: const TextStyle(
+              color: Colors.white,
+              fontFamily: 'vazirbold',
+              fontSize: 16,
+            ),
+            titleLarge: const TextStyle(
+              color: Colors.white,
+              fontFamily: 'vazirbold',
+              fontSize: 30,
+            ),
+            titleMedium: const TextStyle(
+              fontFamily: 'vazirbold',
+              color: Colors.black,
+              fontSize: 16,
+            ),
+            titleSmall: const TextStyle(
+              fontFamily: 'vazirlight',
+              fontSize: 14,
+              color: Colors.black,
+            ),
+            labelSmall: TextStyle(
+                color: CustomColors.mainBlue,
+                fontSize: 14,
+                fontFamily: 'vazirbold')),
         inputDecorationTheme: const InputDecorationTheme(),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
@@ -64,7 +70,7 @@ class App extends StatelessWidget {
       ),
       home: const Directionality(
         textDirection: TextDirection.rtl,
-        child: ProductsByCategoryScreen(),
+        child: BasketScreen(),
       ),
     );
   }
