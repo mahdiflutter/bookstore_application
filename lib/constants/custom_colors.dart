ccde45a07d3567d64fccdcf011bad5820d61a6e1
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-18 13:24:24 +0430

    update colors

--- lib/constants/custom_colors.dart
+++ lib/constants/custom_colors.dart
@@ -6,4 +6,5 @@ class CustomColors {
   static Color mainGreen = const Color(0xff1DB68B);
   static Color mainDark = const Color(0xff121F25);
   static Color mainOrange = const Color(0xffFF725E);
+  static Color mainGrayText = const Color(0xff858585);
 }
