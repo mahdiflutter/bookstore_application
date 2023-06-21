6c71da55fa833e00f656a3fc2f62392ba901b885
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-21 08:54:53 +0430

    create custom math static  class

---
+++ lib/services/service_math.dart
@@ -0,0 +1,13 @@
+import 'package:intl/intl.dart';
+
+class CustomMacth {
+  static int calcuteDiscount(int discount, int realPrice) {
+    var discountPrice = realPrice * discount ~/ 100;
+    return realPrice - discountPrice;
+  }
+
+  static String seperate3Digit(int input) {
+    var formater = NumberFormat('###,###,000');
+    return formater.format(input);
+  }
+}
