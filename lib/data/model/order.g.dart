ee9926d9c167a9bafa8a49e7f013b5a603c6af3a
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-24 13:57:59 +0430

    create order model type adapter

---
+++ lib/data/model/order.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OrderModelAdapter extends TypeAdapter<OrderModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  OrderModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OrderModel(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      store: fields[2] as String,
+      discount: fields[3] as int,
+      realPrice: fields[4] as int,
+      count: fields[5] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OrderModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.store)
+      ..writeByte(3)
+      ..write(obj.discount)
+      ..writeByte(4)
+      ..write(obj.realPrice)
+      ..writeByte(5)
+      ..write(obj.count);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OrderModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
